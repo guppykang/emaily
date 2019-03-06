2d2dc13893e61ea19f234450806268e557633619
Joshua Kang <joshuakang@Joshuas-MacBook-Pro-4.local>
2019-03-06 12:22:43 -0800

    first express node request

---
+++ server/index.js
@@ -0,0 +1,20 @@
+//require allows you to access the dependencies
+//this is  a node.js file
+const express = require('express');
+
+const app = express();
+
+//this creates a route handler. get does this for us
+//the back slash is for the certain route "localhost:5000/" aka root route
+//req is the incoming request
+//res is the response
+//req and res are parameters for the arrow function below
+app.get('/', (req, res) => {
+  res.send({hi : "there"})
+})
+
+//during runtime heroku will tell us which port to use through the env variables
+const PORT  = process.env.PORT || 5000
+
+//express tells node to listen to port 5000
+app.listen(PORT);
