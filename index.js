ecc9795d02a4fdd0a63643002b4b96228d07fe09
Joshua Kang <joshuakang@joshuas-mbp-4.lan>
2019-03-08 02:02:36 -0800

    added passport js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //require allows you to access the dependencies
 //this is  a node.js file
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
@@ -9,12 +11,14 @@ const app = express();
 //req is the incoming request
 //res is the response
 //req and res are parameters for the arrow function below
-app.get('/', (req, res) => {
-  res.send({hi : "there"})
-})
+// app.get('/', (req, res) => {
+//   res.send({hi : "there"})
+// })
+
+passport.use(new GoogleStrategy());
 
 //during runtime heroku will tell us which port to use through the env variables
-const PORT  = process.env.PORT || 5000
+const PORT  = process.env.PORT || 5000;
 
 //express tells node to listen to port 5000
 app.listen(PORT);
