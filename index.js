fdc9a751e4964264f012f01768addc8e01df03a6
Joshua Kang <joshuakang@Joshuas-MacBook-Pro-6.local>
2019-03-25 22:46:21 -0700

    finished auth without any UI

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 //require allows you to access the dependencies
 //this is  a node.js file
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //importing the auth routes and calling the function on the app as a paramteter
 require('./routes/authRoutes')(app);
+
+//mongoose.connect(keys.mongoURI);
 //during runtime heroku will tell us which port to use through the env variables
 const PORT  = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
 const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
+
+
+passport.serializeUser((user,done) => {
+  done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(
   new GoogleStrategy({
@@ -9,9 +26,17 @@ passport.use(
     callbackURL: '/auth/google/callback',
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   }, (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      User.findOne({googleId: profile.id})
+        .then((existingUser) => {
+          if(existingUser){
+            done(null, existingUser);
+          }
+          else {
+            new User({googleId: profile.id}).save()
+              .then(user => done(null, user));
+          }
+        })
+
 
   })
 );
