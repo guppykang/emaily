2121d8f2ae01011cddc2676a89b000ff8987e31f
Joshua Kang <joshuakang@Joshuas-MacBook-Pro-6.local>
2019-03-29 17:20:16 -0700

    yer mama

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,9 +7,7 @@ module.exports = app => {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
 
-  app.get('/', (req, res) => {
-    res.send({hi : "mom"})
-  })
+  
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
